c24e8c78673e2d7e214f323c0e952ec038308a69
D_Ivan <JamesDeht@gmail.com>
2019-12-15 14:11:18 +0200

    Initial Commit

---
+++ FlickrClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrClient
+//
+//  Created by Tyts on 15.12.2019.
+//  Copyright © 2019 Tyts&Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
